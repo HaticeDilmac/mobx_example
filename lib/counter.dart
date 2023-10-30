49506b6aeb9e5bda1c71ad1538e349da658d5136
Hatice Dilmac <hdilmac@tederga.com>
2023-10-30 16:39:32 +0300

    first commit

---
+++ lib/counter.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:mobx/mobx.dart';
+
+// Include generated file
+part 'counter.g.dart';
+
+// This is the class used by rest of your codebase
+class Counter = _Counter with _$Counter;
+
+// The store-class
+abstract class _Counter with Store {
+  @observable
+  int value = 0;
+
+  @action
+  void increment() {
+    value++;
+  }
+}
